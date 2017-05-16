3af6762c232b3761f88fcbebdd6b98875b31f6ee
Renato Lessa <renatolessa.2011@hotmail.com>
2017-05-16 17:14:39 -0300

    Unificando Projeto

---
+++ manutencao.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="pt-br" />
+    
+   	<meta name="author" content="Tarso de Castro">
+	<meta name="reply-to" content="tarsodecastro@gmail.com">
+	<meta name="revised" content="Tarso de Castro, 14/09/2016" />
+	<meta name="description" content="GeraDoc - Gerenciamento de documentos oficiais padronizados com facilidade. Mais informações em http://www.geradox.com.br/">
+	<meta name="abstract" content="GeraDoc">
+	<meta name="keywords" content="sistema em manutenção">
+	<meta name="ROBOT" content="Index,Follow">
+    
+
+	<title>Manutenção</title>
+</head>
+<body>
+
+<center>
+<br />
+<h1>Sistema em manutenção.</h1>
+<h2>Aguarde...</h2>
+</center>
+
+
+</body>
+</html>
